27bcba185a8855dbf16799a5a3a1f77cf7c6270b
Manuel Cano <isc.juan.manuel.cr@gmail.com>
2023-11-16 02:01:19 -0600

    Creación de proyecto base

---
+++ src/main/java/com/jmcr/consumomarveljar/ConsumoMarvelJarApplication.java
@@ -0,0 +1,13 @@
+package com.jmcr.consumomarveljar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoMarvelJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoMarvelJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmcr/consumomarveljar/ConsumomarveljarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmcr.consumomarveljar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumomarveljarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
